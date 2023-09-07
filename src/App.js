95319d9ba04c10dc11917f29fa0c599fc168e64d
Evgeniy Shapechko <shapen86@mail.ru>
2023-09-07 22:08:15 +0300

    add eslint-plugin-react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 function App() {
-    return (
-        <header className="App-header">
-            <p>Hello React</p>
-        </header>
-    );
+  return (
+    <header className='App-header'>
+      <p>Hello React</p>
+    </header>
+  );
 }
 
 export default App;
