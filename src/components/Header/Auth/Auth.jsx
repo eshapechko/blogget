e669f15fbac9a5ebc861d6feec0ecdd0409dce8c
Evgeniy Shapechko <shapen86@mail.ru>
2023-09-11 12:35:45 +0300

    add prop-types in project

--- src/components/Header/Auth/Auth.jsx
+++ src/components/Header/Auth/Auth.jsx
@@ -1,4 +1,5 @@
 import style from './Auth.module.css';
+import PropTypes from 'prop-types';
 
 export const Auth = ({auth}) => (
   <button className={style.button}>
@@ -41,3 +42,7 @@ export const Auth = ({auth}) => (
     )}
   </button>
 );
+
+Auth.propTypes = {
+  auth: PropTypes.bool,
+};
