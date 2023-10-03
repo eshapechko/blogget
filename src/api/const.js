749eca88a683b8e71e1449844d5fb88f5656b23e
Evgeniy Shapechko <shapen86@mail.ru>
2023-10-03 19:56:57 +0300

    fix const REDIRECT_URI

--- src/api/const.js
+++ src/api/const.js
@@ -3,5 +3,5 @@ export const URL_AUTH = 'https://www.reddit.com/api/v1/authorize?';
 export const CLIENT_ID = '7YSfV3rdyNuPx8AkGu0fCw';
 export const RESPONSE_TYPE = 'token';
 export const RANDOM_STRING = 'random_string';
-export const REDIRECT_URI = 'http://localhost:3000/auth';
+export const REDIRECT_URI = 'https://blogget-alpha.vercel.app/auth';
 export const SCOPE = 'identity submit read';
